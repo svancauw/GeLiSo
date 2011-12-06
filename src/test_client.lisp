ff3201d544e13aa36570440d09310e9c1dbdb32a
svancauw <sascha.vancauwelaert@gmail.com>
2011-12-06 16:03:27 +0100

    Forgotten Lisp client added

---
+++ src/test_client.lisp
@@ -0,0 +1,38 @@
+(require "comm")
+
+;start-up-server
+
+;process-kill
+
+;open-tcp-stream
+
+;socket-stream
+
+;(with-open-stream ...)
+
+;	read-sequence
+;write-sequence
+
+;make-array
+
+;wait-for-input-streams
+
+(defun connection_test ()
+	;OK !
+	;(comm:open-tcp-stream "127.0.0.1" 2222)
+
+
+	(with-open-stream 
+		(tcp_str 
+	    	(comm:open-tcp-stream "localhost" 2222 :element-type 'base-char)
+		)
+		(let 
+			((res (make-array 128 :element-type 'base-char)))
+			(values (read-sequence res tcp_str) res)
+		)
+	)
+		
+	
+	
+)
+
