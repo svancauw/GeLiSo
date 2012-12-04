3cda89472c998e2f2c74da5f9bc48dfc2bf007ff
Sascha Van Cauwelaert <sascha.vancauwelaert@uclouvain.be>
2012-12-04 14:48:26 +0100

    GeLiSo can now normally be installed on all machines (only tested on mac)

--- lispsources/load.lisp
+++ lispsources/load.lisp
@@ -1,6 +1,10 @@
 ;loader
+;load this file to be able to use GeLiSo from Lisp (e.g., (load "/Users/saschavancauwelaert/Documents/EPL/PhD/code/GeLiSo/GeLiSo/lispsources/load.lisp"))
+;currently, it must be done after having launched the C++ application of GeLiSo (i.e., build/bin/gelisoexec)
 
-(change-directory "/Users/saschavancauwelaert/Documents/EPL/PhD/code/GeLiSo_interface/GeLiSo/lispsources/")
+;change current directory to GeLiSo lisp source directory
+(change-directory "/Users/saschavancauwelaert/Documents/EPL/PhD/code/GeLiSo/GeLiSo/lispsources/")
+;(change-directory "/Users/saschavancauwelaert/Documents/EPL/PhD/code/GeLiSo_interface/GeLiSo/lispsources/")
 
 ;load the needed files
 (load "channel.lisp")
--- lispsources/main.lisp
+++ lispsources/main.lisp
@@ -1,16 +1,19 @@
-;to use this file :
-;(load "/Users/saschavancauwelaert/Documents/EPL/PhD/code/GeLiSo_interface/GeLiSo/lispsources/main.lisp")
-;(main)
+;load this file to test GeLiSo from Lisp (e.g., (load "/Users/saschavancauwelaert/Documents/EPL/PhD/code/GeLiSo/GeLiSo/lispsources/main.lisp"))
+;currently, it must be done after having launched the C++ application of GeLiSo (i.e., build/bin/gelisoexec)
+;then call (main /Path/To/GeLiSo/Dir/lispsources) where /Path/To/GeLiSo/Dir/ is the path where you have installed GeLiSo
+;e.g., (main "/Users/saschavancauwelaert/Documents/EPL/PhD/code/GeLiSo/GeLiSo/lispsources/")
 
-(defun main ()
+(defun main (lispSourceDir)
 	
-	;launch the C++ ser	ver (don't work well)
+	;TODO : launch the C++ server (don't work well)
 	;(system:run-shell-command "/Users/saschavancauwelaert/Documents/EPL/PhD/code/GeLiSo_interface/build/bin/gelisoexec")
 	;(sleep 5)
 	;(print "Now we begin")
 	
 	;go in the source directory
-	(change-directory "/Users/saschavancauwelaert/Documents/EPL/PhD/code/GeLiSo_interface/GeLiSo/lispsources/")
+	;(change-directory "/Users/saschavancauwelaert/Documents/EPL/PhD/code/GeLiSo_interface/GeLiSo/lispsources/")
+	;(change-directory "/Users/saschavancauwelaert/Documents/EPL/PhD/code/GeLiSo/GeLiSo/lispsources/")
+	(change-directory lispSourceDir)
 	
 	;load the needed files
 	(load "channel.lisp")
